cbd29c544b5b6792c3afe04b314b4e503801ec38
LucasHenrique26122005 <lucashenrique.unip.webresponsiva@gmail.com>
2024-05-16 10:54:05 -0300

    Subir os arquivos do meu site

---
+++ JAVA (1).js
@@ -0,0 +1,19 @@
+
+function callLink1(){
+    window.location = "ORIGEM2.html"
+    
+}
+
+function callLink2(){
+    window.location = "LEVANTAMENTO.html"
+    
+}
+
+function callLink3(){
+    window.location = "Untitled-1.html"
+    
+}
+function callLink4(){
+    window.location = "aps (1).html"
+    
+}
